bda3066d61c27d24fd9b9f3c1441af85f0d511e0
Filipe <filipe@websis>
2021-09-18 10:42:19 -0300

    testando

--- common/environment.ts
+++ common/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     server: { port: process.env.PORT || 3000},
-    db: {url: process.env.URL_DB || 'mongodb://localhost/marketplace-manager'},
+    db: {url: process.env.MONGODB_URI || 'mongodb://localhost/marketplace-manager'},
     security: {
         saltRounds: process.env.SALT_ROUND || 11,
         apiSecret: process.env.API_SECRET || 'marketplace-api-secret'
