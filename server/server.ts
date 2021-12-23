c74960313ac74528ac7bd2765d255638ddfc40b9
Filipe <filipe@websis>
2021-12-23 12:45:04 -0300

    Mais um teste CORS

--- server/server.ts
+++ server/server.ts
@@ -40,8 +40,8 @@ export class Server{
 
                 this.application.use(function (req, res, next) {
                     res.setHeader('Access-Control-Allow-Origin', '*');
-                    res.setHeader('Access-Control-Allow-Methods', '*');
-                    res.setHeader('Access-Control-Allow-Headers', '*');
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, OPTIONS');
+                    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
                     next();
                 });
                 for (let router of routers){
