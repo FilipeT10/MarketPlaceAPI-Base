b5e5582492e53e1d39b09b63527f474cf0a9de74
Filipe <filipe@websis>
2022-03-18 16:22:45 -0300

    Teste sem chaves

--- dist/routes/lojas/lojas.router.js
+++ dist/routes/lojas/lojas.router.js
@@ -69,9 +69,9 @@ class LojasRouter extends model_router_1.ModelRouter {
         return resource;
     }
     applyRoutes(application) {
-        application.get(`${this.basePath}`, (0, authz_handler_1.authorize)('sysAdminMktPlc'), this.findAll);
+        application.get(`${this.basePath}`, (0, authz_handler_1.authorize)('admin'), this.findAll);
         application.get(`${this.basePath}/:id`, [this.validateId, this.findById]);
-        application.post(`${this.basePath}`, [(0, authz_handler_1.authorize)('sysAdminMktPlc'), this.saveLoja]);
+        application.post(`${this.basePath}`, (0, authz_handler_1.authorize)('sysAdminMktPlc'), this.saveLoja);
         application.put(`${this.basePath}/:id`, [this.validateId, (0, authz_handler_1.authorize)('sysAdminMktPlc'), this.replace]);
         application.patch(`${this.basePath}/:id`, [this.validateId, (0, authz_handler_1.authorize)('sysAdminMktPlc'), this.update]);
         application.del(`${this.basePath}/:id`, [this.validateId, (0, authz_handler_1.authorize)('sysAdminMktPlc'), this.delete]);
--- routes/lojas/lojas.router.ts
+++ routes/lojas/lojas.router.ts
@@ -74,9 +74,9 @@ class LojasRouter extends ModelRouter<Loja> {
 
  applyRoutes(application: restify.Server){
 
-     application.get(`${this.basePath}`, authorize('sysAdminMktPlc'), this.findAll)
+     application.get(`${this.basePath}`, authorize('admin'), this.findAll)
      application.get(`${this.basePath}/:id`, [this.validateId, this.findById])
-     application.post(`${this.basePath}`, [ authorize('sysAdminMktPlc'), this.saveLoja])
+     application.post(`${this.basePath}`, authorize('sysAdminMktPlc'), this.saveLoja)
      application.put(`${this.basePath}/:id`, [this.validateId, authorize('sysAdminMktPlc'), this.replace])
      application.patch(`${this.basePath}/:id`, [this.validateId, authorize('sysAdminMktPlc'), this.update])
      application.del(`${this.basePath}/:id`, [this.validateId, authorize('sysAdminMktPlc'), this.delete ])
